b17f6e27c328857e0a5083403fd33aadc2f0d13c
Luan Braz <luan_silvacpv@hotmail.com>
2024-04-07 12:01:26 -0300

    implementing the connection with rabbitmq, consumer and producer

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,13 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserProducer userProducer;
+
     @Transactional
     public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
